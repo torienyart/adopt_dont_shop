15edb5a18843d43420aa1ea6a3684c4ecc020b9b
Tori Enyart <torienyart@gmail.com>
2023-02-14 16:20:53 -0700

    Add table row for status to applciation pets

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2023_02_09_221938) do
   create_table "application_pets", force: :cascade do |t|
     t.bigint "pet_id"
     t.bigint "application_id"
+    t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["application_id"], name: "index_application_pets_on_application_id"
